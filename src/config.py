86819e33624d16ff95d6c1abd855fb7eeec40893
p4el21 <evgeniyandreev21@gmail.com>
2024-09-02 17:41:40 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@ class Config:
     URL = 'https://stellarburgers.nomoreparties.site/'
     TIMEOUT = 2
     NAME = 'Евгений'
-    EMAIL = 'evgeniyandreev10888@mail.ru'
+    EMAIL = 'evgeniyandreev10666@mail.ru'
     PASSWORD = '12345678'
     WRONG_PASSWORD = '12345'
